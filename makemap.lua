b1f708fc15d7a591ee54e8a586a3fe5863d2cc04
Michal Hoftich <michal.h21@gmail.com>
2018-10-30 12:22:19 +0100

    print zotero types

--- makemap.lua
+++ makemap.lua
@@ -1,4 +1,7 @@
 local json = require "json"
+kpse.set_program_name "luatex"
+local domobject = require "luaxml-domobject"
+
 
 local function load_json(filename)
   local f = io.open(filename, "r")
@@ -7,10 +10,19 @@ local function load_json(filename)
   return json.decode(data)
 end
 
+local function load_xml(filename)
+  local f = io.open(filename, "r")
+  if not f then return nil, "Cannot load " .. filename end
+  local data = f:read("*all")
+  local dom = domobject.parse(data)
+  return dom
+end
 local cslfields = load_json("cslfieldstobiblatex.json")
 local languages = load_json("languages.json")
 local pubstates = load_json("pubstates.json")
 local bibtypes  = load_json("bibtypes.json")
-for k,v in pairs(cslfields) do
-  print(k,v)
+local zotero_types = load_xml("zoterotypes.xml")
+
+for _, field in ipairs(zotero_types:query_selector("cslFieldMap map")) do
+  print(field:get_attribute("zfield"), field:get_attribute("cslfield"))
 end
